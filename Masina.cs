f3944cd77ca905d0e72bbcb82d8e4892f28d4438
Florescu Cristian <qflorescucristian@gmail.com>
2023-04-28 20:18:54 +0300

    first commit

---
+++ Masina.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teorie
+{
+    public class Masina
+    {
+        // Atribute 
+        private string _marca;
+        private string _model;
+        private string _numarInmatriculare;
+        private string _numeDetinator;
+        private string _culoare;
+        private int _an;
+        private int _greutate;
+        private int _id;
+
+
+        //Constructor
+
+
+        public Masina()
+        {
+            Console.WriteLine("Eu sunt un constructor fara parametrii");
+        }
+
+        public Masina(string ceva)
+        {
+            Console.WriteLine($"Eu {ceva} sunt un constructor cu un");
+        }
+
+        public Masina(string marca, string model, string numarInmatriculare, string numeDetinator, string culoare, int an, int greutate, int id)
+        {
+            _marca = marca;
+            _model = model;
+            _numarInmatriculare = numarInmatriculare;
+            _numeDetinator = numeDetinator;
+            _culoare = culoare;
+            _an = an;
+            _greutate = greutate;
+            _id = id;
+        }
+
+        // Accesorii
+
+        public string Marca
+        {
+            get { return _marca; }
+            set
+            {
+                if (value.Equals("Dacia"))
+                {
+                    _marca = "undefined";
+                }
+                else
+                {
+                    _marca = value;
+                }
+            }
+        }
+        
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public string NumarInmatriculare
+        {
+            get { return _numarInmatriculare; }
+            set
+            {
+                if (value.Equals("BV00ABC"))
+                {
+                    _numarInmatriculare = "undefined";
+                }
+                else
+                {
+                    _numarInmatriculare = value;
+                }
+            }
+        }
+
+        public string NumeDetinator
+        {
+            get { return _numeDetinator; }
+            set { _numeDetinator = value; }
+        }
+
+        public string Culoare
+        {
+            get { return _culoare; }
+            set
+            {
+                if (value.Equals("mov") || value.Equals("roz"))
+                {
+                    _culoare = "undefined";
+                }
+                else
+                {
+                    _culoare = value;
+                }
+            }
+        }
+
+        public int An
+        {
+            get { return _an; }
+            set
+            {
+                if(value < 2000)
+                {
+                    _an = -1;
+                }
+                else
+                {
+                    _an = value;
+                }
+            }
+        }
+
+        public int Greutate
+        {
+            get { return _greutate; }
+            set 
+            {
+                if (value > 2000)
+                {
+                    _greutate = -1;
+                }
+                else
+                {
+                    _greutate = value;
+                }
+            }
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Describe()
+        {
+            string desc = "";
+            desc += "Marca : " + _marca + "\n";
+            desc += "Model : " + _model + "\n";
+            desc += "Numar Inmatriculare : " + _numarInmatriculare + "\n";
+            desc += "Nume detinator : " + _numeDetinator + "\n";
+            desc += "Culoare : " + _culoare + "\n";
+            desc += "An : " + _an + "\n";
+            desc += "Greutate : " + _greutate + "\n";
+            desc += "Id : " + _id + "\n";
+
+            return desc;
+        }
+    }
+}
---
+++ MasinaService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teorie
+{
+    public class MasinaService
+    {
+
+        private List<Masina> _listMasina;
+
+        public MasinaService()
+        {
+            _listMasina = new List<Masina>();
+
+            this.load();
+        }
+
+        public void load()
+        {
+            Masina m1 = new Masina("Nissan", "Quest", "39926", "Burke Elgie", "Teal", 1995, 1769, 1);
+            Masina m2 = new Masina("Saab", "9000", "43804", "Adoree Aspey", "Puce", 1993, 1457, 2);
+            Masina m3 = new Masina("Lamborghini", "Murciélago", "121101", "Rosemary Leyden", "Mauv", 2010, 1441, 3);
+            Masina m4 = new Masina("GMC", "1500 Club Coupe", "6094", "Seumas Spurret", "Crimson", 1994, 1634, 4);
+            Masina m5 = new Masina("BMW", "745", "48672", "Penn Benkhe", "Mauv", 2003, 1742, 5);
+            Masina m6 = new Masina("Toyota", "Tacoma", "34440", "Thurstan Buterton", "Indigo", 2003, 1644, 6);
+
+            _listMasina.Add(m1);
+            _listMasina.Add(m2);
+            _listMasina.Add(m3);
+            _listMasina.Add(m4);
+            _listMasina.Add(m5);
+            _listMasina.Add(m6);
+        }
+
+        public void afisare()
+        {
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            foreach (Masina m in _listMasina)
+            {
+                Console.WriteLine(m.Describe());
+            }
+        }
+
+    }
+}
